bfd6194e782a537b3b4a6bb307f2917fe9f6d243
Sayam112 <sayam.24bai10591@vitbhopal.ac.in>
2026-03-28 19:06:56 +0530

    Create script4.sh

---
+++ script4.sh
@@ -0,0 +1,13 @@
+Script 4: Network and Host Information
+
+This provides information about the hostname, IP address, and network configuration which helps identify the system
+and checks connectivity settings of the system.
+hostname ( displays the system hostname), ip( dislays ip address and network interface details) are the linux commands used.
+
+Code:
+#!/bin/bash
+
+echo "===== Hostname ====="
+hostname
+
+echo ""
